3e587547f6245f0979aa58fa770b089f0cdd9a38
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-10-09 18:20:11 -0500

    fetch info artist

--- src/components/ArtistGeneral.vue
+++ src/components/ArtistGeneral.vue
@@ -30,6 +30,15 @@ export default {
         .then(function(myJson) {
           console.log(myJson.toptracks.track);
         });
+    },
+    getArtistInfo : function() {
+      fetch('http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=metallica&api_key=8cd7c52e092d2a9a7f15b02486b94879&format=json')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(myJson.toptracks.track);
+        });
     }
   }
 }
